dd00f8dcf4078dcb6ea925ea1f6347624d8c975a
Akash Soti <akashsoti@gmail.com>
2020-04-12 12:08:11 +0530

    remove extras

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-
-
-$(document).on('turbolinks:load', function() {
-  message_appender("hellow a")
-});
-
-message_appender = function(content) {
-  $('#messages-table').append(content);
-}
